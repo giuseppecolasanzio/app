517564045a2e908db8da37a8150476eea7040be4
gcolasanzio <giuseppe.colasanzio@soprasteria.com>
2019-11-04 11:23:30 +0100

    FIRST COMMIT

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '../styles/index.scss';
+
+
+const App = () =>(
+    <div>
+        <h1>Hello world!!!!!!!!!!!!!!!!!!!!!!!!!!</h1>
+    </div>
+);
+
+
+ReactDOM.render(
+    <App/>,
+    document.getElementById('root')
+);
+
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const HWP = require('html-webpack-plugin');
+const {HotModuleReplacementPlugin} = require('webpack');
+
+module.exports = {
+   entry: path.join(__dirname, '/src/index.js'),
+   output: {
+       filename: 'build.js',
+       path: path.join(__dirname, '/dist')},
+   module:{
+       rules:[
+            {
+              test: /\.(js|jsx)$/,
+              exclude: /node_modules/,
+              loader: 'babel-loader',
+              options: {
+                    presets: ['@babel/preset-env', '@babel/preset-react']
+                }
+            },
+            {
+                test: /\.(scss|sass)$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]  
+            }
+        ]
+   },
+   plugins:[
+      new HotModuleReplacementPlugin(),
+      new HWP(
+          {template: path.join(__dirname,'/src/index.html')}
+       )
+   ]
+}
+
