146d6650f0038ba57693bf31d4b8df5a23da7153
gcolasanzio <giuseppe.colasanzio@soprasteria.com>
2019-11-11 14:23:13 +0100

    elevato lo stato dall item al container

--- src/component/container.js
+++ src/component/container.js
@@ -8,13 +8,33 @@ class Container extends React.Component {
         super(props);
         this.state = {
             value: null,
+            item : {
+                x: props.size / 2,
+                css: {left : props.size / 2+'px'}
+            }
         };
+
+        document.addEventListener('keydown', (e) => {
+            console.log(e.code);
+            if (e.code === "ArrowRight")        this.move(10)
+            else if (e.code === "ArrowLeft") this.move(-10)
+        });
+
     }
+
+    move(delta){
+        let newState = {};
+        let x = this.state.item.x;
+        x += delta;
+        newState.x = x;
+        newState.css = {left : x + 'px'};
+        this.setState({item : newState});
+    };
 
     render(){
         return(
             <div className='container' style={this.props.style}>
-                <Item size={this.props.size}/>
+                <Item position={this.state.item}/>
             </div>
         )
     };
--- src/component/item.js
+++ src/component/item.js
@@ -1,33 +1,9 @@
 import React from 'react';
 
 class Item extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            x: props.size / 2,
-            css: {left : props.size / 2+'px'},
-        };
-
-        document.addEventListener('keyup', (e) => {
-            if (e.code === "ArrowRight")        this.move(10)
-            else if (e.code === "ArrowLeft") this.move(-10)
-        });
-
-    };
-
-     move(delta){
-        let newState = {};
-        let x = this.state.x;
-        x += delta;
-        newState.x = x;
-        newState.css = {left : x + 'px'};
-        this.setState(newState);
-    };
-
     render(){
         return(
-            <div className='item' style={this.state.css}>
+            <div className='item' style={this.props.position.css}>
             </div>
         )
     };
