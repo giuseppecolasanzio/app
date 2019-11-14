df99cfc99c742d7bc58d0bfa7f307486ce05c3c3
gcolasanzio <giuseppe.colasanzio@soprasteria.com>
2019-11-14 15:56:48 +0100

    add bullets

--- src/component/container.js
+++ src/component/container.js
@@ -10,44 +10,74 @@ class Container extends React.Component {
         let y = props.size.height - 20;
         this.state = {
             value: null,
+            bullets: [],
             item : {
                 x: x,
-                y: y,
-                css: {
-                    left : `${x}px`,
-                    top: `${y}px`
-                }
+                y: y
             }
         };
 
+        this.clockTimer = setInterval(() => this.clock(), 20);
+
         document.addEventListener('keydown', (e) => {
-            console.log(e.code);
             if (e.code === "ArrowRight") this.move(10)
             else if (e.code === "ArrowLeft") this.move(-10)
         });
 
+        document.addEventListener('keyup', (e) => {
+            if (e.code === "Space") this.shot();
+        });
+
+    }
+
+    componentWillUnmount(){
+        clearInterval(this.clockTimer);
+    }
+
+    shot(){
+
+        let startX = this.state.item.x;
+        let startY = this.state.item.y;
+
+        let newBullets = produce(this.state.bullets, draftBullets =>{
+            draftBullets.push({x:startX, y:startY});
+        });
+
+        this.setState({bullets: newBullets});
+
     }
 
     move(delta){
-
       let nextPosition = this.state.item.x + delta;
       if (nextPosition >= 0 && nextPosition < this.props.size.width) {
 
           let newItem = produce(this.state.item, draftItem => {
               draftItem.x = nextPosition;
-              draftItem.css.left = `${nextPosition}px`;
           });
           this.setState({item: newItem});
 
       }
     };
 
+    clock(){
+        let newBullets = produce(this.state.bullets.filter(b => b.y > 0), draftBullets => {
+                draftBullets.map(b => b.y -= 5);
+            });
+
+        this.setState({ bullets: newBullets });
+
+    }
+
     render(){
         return(
             <div className='container' style={this.props.style}>
 
-                    <Item position={this.state.item}/>
+                {this.state.bullets.map(
+                    (bullet) =>
+                        <div key={`${bullet.x}x${bullet.y}`} className={'bullet'} style={{left : `${bullet.x}px`, top: `${bullet.y}px`}}>0</div>
+                )}
 
+                <Item position={this.state.item}/>
             </div>
         )
     };
--- src/component/item.js
+++ src/component/item.js
@@ -3,7 +3,7 @@ import React from 'react';
 class Item extends React.Component {
     render(){
         return(
-            <div className='item' style={this.props.position.css}>
+            <div className='item' style={{left : `${this.props.position.x}px`, top: `${this.props.position.y}px`}}>
             </div>
         )
     };
